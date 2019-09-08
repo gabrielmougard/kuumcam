feea1d5a3330ba99aca3948bf5d40feec21b85eb
gab <gabriel.mougard@gmail.com>
2019-09-08 20:50:20 +0200

    almost finish Binder

--- python/endpoint/platform.py
+++ python/endpoint/platform.py
@@ -1,7 +1,8 @@
 import requests
 import configparser
-
-import  
+import socket
+import os 
+from subprocess import Popen
 """
 `Binder` handle the I/O operations between the endpoint and the platform.
 """
@@ -13,11 +14,12 @@ class Binder:
     def bind(self):
         if (self.code is not None): #first connection
             #get the URL root of the platform
-            URL_PATH = self.networkParser.get("PLATFORM","FIRST_CONN_ROUTE")
+            URL_PATH_FIRST_CONN = self.networkParser.get("PLATFORM","FIRST_CONN_ROUTE")
             codeStr = ''
+            del code[-1] #delete the VALIDATION character
             codeStr.join(map(str,code)) #now code is string
 
-            r = requests.post(URL_PATH,data= {'code': codeStr})
+            r = requests.post(URL_PATH_FIRST_CONN,data= {'code': codeStr})
             response = r.json() # we'll a JSON as a response from the platform
 
             if (response['authorized']):
@@ -27,8 +29,61 @@ class Binder:
                 with open('config/network.conf') as f_out:
                     self.networkParser.write(f_out)
 
+                
+                if (isOnline() and isUUID(uuid)):
+                    #launch the streaming engine with the right parameters
+                    Popen(['python3','../streaming-engine/kuumcamV2.py',uuid]) #just pass uuid as args[0] in order that the program know where to send the stream 
+                
+
             else:
                 print("binding failed !")
 
         else: # `network.conf` has already been setup 
-            
+            URL_PATH_CONN = self.networkParser.get("PLATFORM","CONN_ROUTE")
+
+            #by security measure, check the UUID in the file and it's integrity
+            uuid = self.networkParser.get("PLATFORM","UUID")
+            if (isUUID(uuid)):
+                r = requests.post(URL_PATH_CONN,data= {'uuid': uuid})
+                response = r.json()
+
+                if (response["authorized"]):
+                    #launch the streaming engine with the right parameters
+                    Popen(['python3','../streaming-engine/kuumcamV2.py',uuid]) #just pass uuid as args[0] in order that the program know where to send the stream 
+                
+                else:
+                    print("connection not authorized")
+
+
+    def isOnline():
+        """
+        check network health
+        """
+        hostname = self.networkParser.get("HEALTH","REMOTE_SERVER")
+        try:
+            host = socket.gethostbyname(hostname)
+            s = socket.create_connection((host, 80), 2)
+            s.close()
+            return True
+        except:
+            pass
+        print("connection offline")
+        return False
+
+    def isUUID(u):
+        """
+        check if the shape of `u` is like an UUID (RFC 4122)
+        i.e : 12345678-1234-5678-1234-567812345678  ==> 128 bits, 32 digits on 4 bits
+        """
+        if (len(u) == 36):
+            for digits in u.split('-'):
+                try:
+                    int(digits)
+                except ValueError:
+                    return False
+            return True
+        print("wrong uuid")
+        return False
+
+
+
