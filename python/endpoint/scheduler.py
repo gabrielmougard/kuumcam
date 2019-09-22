92f090300c9af4aa9a047d3b331e2d03ccf884a9
gab <gabriel.mougard@gmail.com>
2019-09-22 18:56:28 +0200

    MVP for TTP229

--- python/endpoint/scheduler.py
+++ python/endpoint/scheduler.py
@@ -15,16 +15,17 @@ and call the `Binder` class when it's ready to be connected to the platform.
 class Scheduler:
     def __init__(self):
         self.code_q = queue.Queue() # queue containing the digit
+        self.notification_q = queue.Queue()
         self.size_q = 7             # the code is 6 digits and the validation is the seventh character...
 
     def run(self):
         parserKeypad = configparser.RawConfigParser()
         parserKeypad.read('config/ttp229-keypad.conf')
-        read_config(parserKeypad)
+        self.__read_config(parserKeypad)
 
-        pollWorker = threading.Thread(target=pollKeys, args=(0, self.size_q, self.code_q))
-        keyboardListener = threading.Thread(target=keysProducer,args=(1,self.code_q))
-
+        pollWorker = threading.Thread(target=self.__pollKeys, args=(0, self.size_q, self.code_q,self.notification_q))
+        keyboardListener = threading.Thread(target=self.__keysProducer,args=(1,self.code_q))
+        notificationWorker = threading.Thread(target=self.__notificationPollWorker,args=(2,self.notification_q))
         #check for existing network connection
         parserNetwork = configparser.RawConfigParser()
         parserNetwork.read('config/network.conf')
@@ -33,57 +34,83 @@ class Scheduler:
 
         pollWorker.start()
         keyboardListener.start()
+        notificationWorker.start()
 
         keyboardListener.join()
         pollWorker.join()
+        notificationWorker.join()
 
-    def pollKeys(self,id,size,in_q):
+    def __pollKeys(self,id,size,in_q,notification_q):
         code = [] #contain the code to be sent to the platform
-
+        boardMapping = {1:1,2:2,3:3,4:"correction",5:4,6:5,7:6,8:"cancel",9:7,10:8,11:9,12:"validate",14:0}
         while True:
-            if ((len(code) == 7) and code[-1] == VALIDATION): #we have 6 digits and the last character is the validation one.
-                platformBinder = Binder(code=code) #create a `Binder` object for platform connection
+            if (not in_q.empty()):
+                key = in_q.get()
+                if key == 13 or key == 15 or key == 16:
+                    mappedKey = -1
+                else:
+                    mappedKey = boardMapping[key]
+                if (mappedKey == "validate"): #error : the code must be 6 digits before being validated
+                    #TODO : handle the error case where codeis not length 6
+                    if len(code) < 6:
+                        print("code must be 6 digits")
+                        code = []
+                        #clear the queue
+                        with in_q.mutex:
+                            in_q.queue.clear()
+                        notification_q.put("cancel")
+                    else:
+                        print("validated !")
+                        print(code)
+                        notification_q.put("validate")
+                        # bind to platform code
+                        #platformBinder = Binder(code=code) #create a `Binder` object for platform connection
 
-                try:
-                    platformBinder.bind() #if the bind is successful, it return a new conf to be saved on the system which will be used for automated connection if the camera if switch off
+                        #try:
+                        #    platformBinder.bind() #if the bind is successful, it return a new conf to be saved on the system which will be used for automated connection if the camera if switch off
 
-                except BindingKuumbaError: #https://www.programiz.com/python-programming/user-defined-exception
-                    #TODO : if an error occur during the binding
-                    print("Kuumba binding error")
+                        #except BindingKuumbaError: #https://www.programiz.com/python-programming/user-defined-exception
+                        #TODO : if an error occur during the binding
+                        #    print("Kuumba binding error")            
+                        #
+                        code = []
+                elif (mappedKey == "correction"):
+                    #notification_q.put(key)
+                    if (len(code) > 0):
+                        keyToDelete = code[-1]
+                        del code[-1] #delete the last one
+                        print("key : "+str(keyToDelete)+" deleted.")
+                    else:
+                        print("nothing to delete")
+                    notification_q.put("correction")
 
-                #clean the queue (thread-safe)
-                with in_q.mutex:
-                    in_q.queue.clear()
-                print("Queue cleared due to complete validated message")
+                elif (mappedKey == "cancel"):
+                    notification_q.put("cancel")
+                    code = [] #empty it
+                    #clear the queue
+                    with in_q.mutex:
+                        in_q.queue.clear()
+                    
+                elif (mappedKey == -1):
+                    print("unused key")
+                else:
+                    notification_q.put(mappedKey)
+                    code.append(mappedKey)
+                    print(str(mappedKey)+" appended to queue")
+                    if len(code) == 7:
+                        print("too much digits.")
+                        notification_q.put("cancel")
+                        code = []
+                        #clear the queue
+                        with in_q.mutex:
+                            in_q.queue.clear()
+            time.sleep(HALF_BIT_TIME) #slowing down the process to avoid perf drop
 
-            else: #retreive char in the queue while checking if it's empty and append the data to the `code` list
-                if (not in_q.empty()):
-                    key = in_q.get()
-
-                    if (key == VALIDATION and len(code) < 6): #error : the code must be 6 digits before being validated
-                        #TODO : handle this error case
-                    elif (key == CORRECTION):
-                        if (len(code) > 0):
-                            del code[-1] #delete the last one
-
-                    elif (key == CANCEL):
-                        if (len(code) > 0): #if the list is not empty
-                            code = [] #empty it
-
-                    else: # a digit
-                        code.append(key)
-            time.sleep(POLL_KEYS_DELAY) #slowing down the process to avoid perf drop
-
-    def keysProducer(self,id,out_q):
-
-        #Config keyboard (the hardcoded values with be replaced by a call to a config file)
-        GPIO.setMode(GPIO.BCM)
+    def __keysProducer(self,id,out_q):
+        #Config keyboard the hardcoded values with be replaced by a call to a config file)
         CHARACTER_DELAY=5*HALF_BIT_TIME
 
-        GPIO.setup(SCLPin,GPIO.OUT)
-        GPIO.setup(SDOPin,GPIO.IN)
-
-        GPIO.output(SCLPin,GPIO.HIGH)
+        GPIO.output(SCL,GPIO.HIGH)
         time.sleep(HALF_BIT_TIME)
         oldKey=18
         #
@@ -92,43 +119,43 @@ class Scheduler:
 
         while True:
             key=1
-    		time.sleep(CHARACTER_DELAY)
+            time.sleep(CHARACTER_DELAY)
 
-            while button < 17:
+            while key < 17:
                 sendedKey=key
                 if (sendedKey == 17):
                     sendedKey = 1
 
-                GPIO.output(SCLPin,GPIO.LOW)
-    			time.sleep(HALF_BIT_TIME)
-    			keyval=GPIO.input(SDOPin)
+                GPIO.output(SCL,GPIO.LOW)
+                time.sleep(HALF_BIT_TIME)
+                keyval=GPIO.input(SD0)
 
                 if not keyval:
                     pressed=True
-                    if (oldKey != key):
-                        end = time.time()
-                        if (end-start >= 10): #cleaning mechanism enabled above 10 seconds for the queue (thread-safe way)
-                            with out_q.mutex:
-                                out_q.queue.clear()
+                    end = time.time()
+                    if (oldKey != key) or end-startTime > 0.3:
+
+                        if (end-startTime >= 10): #cleaning mechanism enabled above 10 seconds for the queue (thread-safe way)
+                            out_q.put(CANCEL) #indicating that we want to delete code
                             print("Queue cleared due to keyboard inactivity")
-                            visualNotification("clear") # notify the user with some LEDs animation (clearing message)
+                            startTime = time.time()
                         else: #just append the queue
                             out_q.put(sendedKey) # send it to the queue for being consummed by the pollKeys worker
-                            print("key #"+str(sendedKey)+" sended to queue.")
-                            visualNotification("key",key=sendedKey) # notify the user with some LEDs animation (for the specified key)
+                            startTime = time.time()
                         oldKey=key
 
-                GPIO.output(SCLPin,GPIO.HIGH)
+                GPIO.output(SCL,GPIO.HIGH)
                 time.sleep(HALF_BIT_TIME)
                 key += 1
 
             pressed=False
 
-    def visualNotification(code,key=None,interruptPending=None):
+    def __visualNotification(self,code,key=None,interruptPending=None):
         """
         LED animation for notifying the user according to the specified code (we will have 3 LEDs on the prototype : RED, YELLOW, GREEN)
         Ultimately, we should have only one RGB led to do all the color that we want.
         """
+
         if (code == "init"):
 
             start = time.time()
@@ -140,79 +167,102 @@ class Scheduler:
                     GPIO.output(YELLOW,GPIO.LOW)
                     GPIO.output(RED,GPIO.HIGH)
                     break
-
                 GPIO.output(GREEN,GPIO.HIGH)
                 GPIO.output(YELLOW,GPIO.LOW)
                 GPIO.output(RED,GPIO.LOW)
-
                 time.sleep(LED_BLINKING)
-
                 GPIO.output(GREEN,GPIO.LOW)
                 GPIO.output(YELLOW,GPIO.HIGH)
                 GPIO.output(RED,GPIO.LOW)
-
                 time.sleep(LED_BLINKING)
-
                 GPIO.output(GREEN,GPIO.LOW)
                 GPIO.output(YELLOW,GPIO.LOW)
                 GPIO.output(RED,GPIO.HIGH)
-
                 time.sleep(LED_BLINKING)
-
                 GPIO.output(GREEN,GPIO.LOW)
                 GPIO.output(YELLOW,GPIO.HIGH)
                 GPIO.output(RED,GPIO.LOW)
-
                 time.sleep(LED_BLINKING)
-
-        elif (code == "clear"):
-            #TODO : LEDs animation for queue/keyboard clearing
-            #just switch off all the LEDs (just like a "sleep" mode)
-            GPIO.output(GREEN,GPIO.LOW)
-            GPIO.output(YELLOW,GPIO.LOW)
-            GPIO.output(RED,GPIO.LOW)
 
         elif (code == "pending"): #need to be executed in a new thread ==> threading.Thread(target=visualNotification,args=("pending")).start() 
             #TODO : LEDs animation for trying to reach the platform once the code is entered  
             while (interruptPending is None):
                 GPIO.output(YELLOW,GPIO.HIGH)
                 time.sleep(LED_BLINKING)
+                GPIO.output(YELLOW,GPIO.LOW)
+                time.sleep(LED_BLINKING)
+            GPIO.output(YELLOW,GPIO.LOW) #when interrupt signal occurs
 
         else: #meaning it's "key" and that key=sendKey
             #TODO : LEDs animation for key typing
-            if (key == VALIDATION):
+            if (key == "validate"):
+                GPIO.output(GREEN,GPIO.HIGH)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.LOW)
+                time.sleep(LED_BLINKING)
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.LOW)
 
-            elif (key == CORRECTION):
+            elif (key == "correction"):
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.HIGH)
+                GPIO.output(RED,GPIO.LOW)
+                time.sleep(LED_BLINKING)            
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.LOW)
 
-            elif (key == CANCEL):
+            elif (key == "cancel"):
+                GPIO.output(GREEN,GPIO.HIGH)
+                GPIO.output(YELLOW,GPIO.HIGH)
+                GPIO.output(RED,GPIO.HIGH)
+                time.sleep(LED_BLINKING)
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.LOW)
 
             else: #digits
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.HIGH)
+                time.sleep(LED_BLINKING)
+                GPIO.output(GREEN,GPIO.LOW)
+                GPIO.output(YELLOW,GPIO.LOW)
+                GPIO.output(RED,GPIO.LOW)
 
-    def shutdown(self):
-        print("keyboard Pub/Sub engine shutdown...")
+    def __notificationPollWorker(self,id,notification_q):
+        WORKER_DELAY = 0.001
+        while True:
+            if (not notification_q.empty()):
+                key = notification_q.get()
+                self.__visualNotification("key",key)
+            time.sleep(WORKER_DELAY)
 
-    def read_config(parser):
-        global GREEN,YELLOW,RED,SCLPin,SDOPin,HALF_BIT_TIME,NUM_BITS,VALIDATION,CORRECTION,CANCEL,POLL_KEYS_DELAY,LED_BLINKING
+    def __read_config(parser):
+        global GREEN,YELLOW,RED,SCL,SD0,HALF_BIT_TIME,LED_BLINKING
 
         #parse config file (PINS)
         GREEN  = parser.getint("PINS","GREEN")
         YELLOW = parser.getint("PINS","YELLOW")
         RED    = parser.getint("PINS","RED")
-        SCLPin = parser.getint("PINS","SCL_PIN")
-        SDOPin = parser.getint("PINS","SDO_PIN")
+        BLUE   = parser.getint("PINS","BLUE")
+
+        SCL = parser.getint("PINS","SCL_PIN")
+        SD0 = parser.getint("PINS","SDO_PIN")
 
         #parse config file (tempo)
         HALF_BIT_TIME   = parser.getfloat("TEMPO","HALF_BIT_TIME")
-        POLL_KEYS_DELAY = parser.getfloat("TEMPO","POLL_KEYS_DELAY")
         LED_BLINKING    = parser.getfloat("TEMPO","LED_BLINKING")
 
-        #parse config file (special keys)
-        VALIDATION = parser.getint("KEYS","VALIDATION")
-        CORRECTION = parser.getint("KEYS","CORRECTION")
-        CANCEL     = parser.getint("KEYS","CANCEL")
-
-        #parse config file (number of bits)
-        NUM_BITS = parser.getint("GLOBAL","NUM_BITS")
+        #pin config
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(GREEN,GPIO.OUT)
+        GPIO.setup(YELLOW,GPIO.OUT)
+        GPIO.setup(RED,GPIO.OUT)
+        GPIO.setup(BLUE,GPIO.OUT)
+        GPIO.setup(SCL,GPIO.OUT)
+        GPIO.setup(SD0,GPIO.IN)
 
     def checkConnection(parser):
         """
