99b01f0fef346b3f7828e027f74c3dfc8fcbec1c
gab <gabriel.mougard@gmail.com>
2019-09-23 12:26:44 +0200

    begin testServer

---
+++ python/testServer/server.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask import request 
+from flask import jsonify
+import random 
+import queue
+import time 
+
+IPDATA_API_KEY = "726f1a11992e5894aa77aa2cd300d24444e9cad58ec347d511e64370" #for geolocalisation purpose (free plan => 1500 API call/month)
+global message_q 
+message_q = dict() #store the code with the timestamp
+
+app = Flask(__name__)
+ 
+@app.route('/', methods=['GET']) #in the web browser
+def generate_code():
+    visitorIP = jsonify({'ip':request.remote_addr})
+    IPDATA_ENDPOINT ="https://api.ipdata.co/"+visitorIP+"?api-key="+IPDATA_API_KEY
+    rawGeoData = requests.get(IPDATA_ENDPOINT).content
+    filteredGeoData = [rawGeoData["city"],rawGeoData["region"],rawGeoData["country_name"],rawGeoData["latitude"],rawGeoData["longitude"],rawGeoData["postal"]]
+
+    code = ""
+    codeLength = 6
+    for i in range(codeLength):
+        code += random.randint(0,9)
+
+    message_q[code] = [time.time(),filteredGeoData]
+    #Save the generated code in a queue with the timestamp
+    #Expire in 1 min after generation.
+    return code
+
+@app.route('/bind',methods=['POST']) #called by the endpoint
+def binding():
+    '''
+    if everything is right, return UUID and notify the backend
+    that a new route must be created paired with this UUID.
+    Else, just return error message.
+    '''
+    #recover the request
+    data = request.get_json()
+    sendedCode = data['code']
+    endpointGeoData = data['geo'] #json containing the same structure as in `generate_code` function.
+
+    if sendedCode in message_q.keys():
+        if time.time() - message_q[sendedCode] < 60:
+            # check geocalisation data
+            if checkGeoData(message_q[sendedCode][1],endpointGeoData):
+                return "code validated !"
+
+            return "wrong geolocalisation !"
+
+        else:
+            #expired
+            return "code expired !"
+
+        del message_q[sendedCode] #delete this element
+    else:
+        # not the good code
+        print("code not validated !")
+
+
+def checkGeoData(webclientData,endpointData):
+    """
+    both parameters are list of the same structure
+    [0] -> city
+    [1] -> region
+    [2] -> country_name
+    [3] -> latitude
+    [4] -> longitude
+    [5] -> postal
+    """
+    if abs(webclientData[3]-endpointData[3]) <= 1e-3 and abs(webclientData[4]-endpointData[4]) <= 1e-3:
+        return True
+    return False
